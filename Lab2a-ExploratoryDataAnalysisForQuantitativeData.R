20579dc4477eaedbeee6fc36f313d21476736c89
course-files <137632706+course-files@users.noreply.github.com>
2023-09-21 08:20:18 +0300

    Updated markdown submission instructions
    Co-Authored-By: Course Files <137632706+course-files@users.noreply.github.com>

--- Lab2a-ExploratoryDataAnalysisForQuantitativeData.R
+++ Lab2a-ExploratoryDataAnalysisForQuantitativeData.R
@@ -900,5 +900,5 @@ renv::snapshot()
 # Documentation of knitR: https://www.rdocumentation.org/packages/knitr/
 
 # Upload *the link* to "Lab-Submission-Markdown.md" (not .Rmd)
-# markdown file hosted on Github (do not upload the .Rmd or .md markdown files)
-# through the submission link provided on eLearning.
+# markdown file hosted on Github's main branch (do not upload the .Rmd or .md
+# markdown files) through the submission link provided on eLearning.
